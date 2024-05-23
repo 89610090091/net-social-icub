76ab978c8451b33ec82ac390484a9b980b3f2227
Alex_Brand <rzn.russia4912@gmail.com>
2024-05-23 21:35:41 +0300

    add_module.css 	modified:   src/componets/Header/Header.module.css 	modified:   src/componets/Navbar/Navbar.module.css 	modified:   src/componets/Profile/Profile.tsx

--- src/componets/Profile/Profile.tsx
+++ src/componets/Profile/Profile.tsx
@@ -7,7 +7,15 @@ const Post = () => {
             <div>
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFSHzVn5cWMiZSrj80a00wyZaMA5rrtwYgagbOFCg1nCp1OHLF-uBL51qn8S_dKc3XPtg&usqp=CAU" alt="" />
             </div>
-            <div>ava + desction</div>
+            <div>ava + description</div>
+            <div>
+                MyPost
+                <div>New Post</div>
+                <div>
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
+            </div>
         </div>
     )
 }
