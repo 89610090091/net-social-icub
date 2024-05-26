4997ad3481449853ade7b2f135b8a20f7c14450a
Alex_Brand <rzn.russia4912@gmail.com>
2024-05-26 14:00:55 +0300

    add_class_app-wrapper-content

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { Profiler } from 'react';
 import './App.css';
 import Header from './componets/Header/Header';
 import Navbar from './componets/Navbar/Navbar';
-import Profile from './componets/Profile/Profile';
+// import Profile from './componets/Profile/Profile';
 import Dialogs from './componets/Dialogs/Dialogs';
 
 function App() {
